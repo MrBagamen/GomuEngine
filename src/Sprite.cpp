1a34c2a4fe80773309524df64b71a469bdeb9686
Mika Attila <radiantstatue@gmail.com>
2014-02-24 22:45:57 +0100

    Refactor Sprite class a bit

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -3,64 +3,49 @@
 namespace gomu
 {
 
-Sprite::Sprite() :
-    texcoord
-    {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f
-    },
-    sprite_color
-    {
-        1.0f, 1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f, 1.0f
-    }
+Sprite::Sprite(const Texture &texture)
 {
-    x = 0;
-    y = 0;
-    angle = 0;
+    setTexture(texture);
 }
 
 void Sprite::draw()
 {
     glPushMatrix();
     m_texture->bind();
-    glTranslatef(x+m_texture->getWidth()/2, y+m_texture->getHeight()/2, 0.0f);
-    glRotatef(angle, 0.0f, 0.0f, 1.0f);
-    glVertexPointer(2, GL_FLOAT, 0, quad);
-    glColorPointer(3, GL_FLOAT, 0, sprite_color);
-    glTexCoordPointer(2, GL_FLOAT, 0, texcoord);
+    glTranslatef(m_x+m_texture->getWidth()/2, m_y+m_texture->getHeight()/2, 0.0f);
+    glRotatef(m_angle, 0.0f, 0.0f, 1.0f);
+    glVertexPointer(2, GL_FLOAT, 0, m_quads);
+    glColorPointer(3, GL_FLOAT, 0, m_colors);
+    glTexCoordPointer(2, GL_FLOAT, 0, m_texcoords);
     glDrawArrays(GL_QUADS, 0, 8);
     glPopMatrix();
 }
 
-void Sprite::rotate(int _angle)
+void Sprite::setAngle(int angle)
 {
-    angle = _angle;
+    m_angle = angle;
 }
 
-void Sprite::color(Uint8 _r, Uint8 _g, Uint8 _b)
+void Sprite::setColor(Uint8 r, Uint8 g, Uint8 b)
 {
-    sprite_color[0] = sprite_color[3] = sprite_color[6] = sprite_color[9] = _r / 255.0f;
-    sprite_color[1] = sprite_color[4] = sprite_color[7] = sprite_color[10] = _g / 255.0f;
-    sprite_color[2] = sprite_color[5] = sprite_color[8] = sprite_color[11] = _b / 255.0f;
+    m_colors[0] = m_colors[3] = m_colors[6] = m_colors[9] = r / 255.0f;
+    m_colors[1] = m_colors[4] = m_colors[7] = m_colors[10] = g / 255.0f;
+    m_colors[2] = m_colors[5] = m_colors[8] = m_colors[11] = b / 255.0f;
 }
 
 void Sprite::setTexture(const Texture &texture)
 {
     m_texture = &texture;
-    quad[0] =-m_texture->getWidth()/2;  quad[1] =-m_texture->getWidth()/2;
-    quad[2] = m_texture->getWidth()/2;  quad[3] =-m_texture->getWidth()/2;
-    quad[4] = m_texture->getWidth()/2;  quad[5] = m_texture->getWidth()/2;
-    quad[6] =-m_texture->getWidth()/2;  quad[7] = m_texture->getWidth()/2;
+    m_quads[0] =-m_texture->getWidth()/2;  m_quads[1] =-m_texture->getWidth()/2;
+    m_quads[2] = m_texture->getWidth()/2;  m_quads[3] =-m_texture->getWidth()/2;
+    m_quads[4] = m_texture->getWidth()/2;  m_quads[5] = m_texture->getWidth()/2;
+    m_quads[6] =-m_texture->getWidth()/2;  m_quads[7] = m_texture->getWidth()/2;
 }
 
-void Sprite::setPosition(int _x, int _y)
+void Sprite::setPosition(int x, int y)
 {
-    x = _x;
-    y = _y;
+    m_x = x;
+    m_y = y;
 }
 
 }
